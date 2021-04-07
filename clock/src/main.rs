ec30b9dce32edc63043c7eda084e7e383c6c6b23
Benoît Peccatte <benoit.peccatte@normation.com>
2021-04-07 11:46:58 +0200

    Display time withing a thread

--- clock/src/main.rs
+++ clock/src/main.rs
@@ -1,25 +1,35 @@
-use std::thread;
-use std::sync::mpsc::channel;
-use std::time::*;
-use std::thread::sleep;
 use chrono::Local;
 use std::sync::mpsc;
+use std::sync::mpsc::channel;
+use std::thread;
+use std::thread::sleep;
+use std::time::*;
+use std::sync::{Arc, Mutex};
+
+use rppal::gpio::Gpio;
+mod display;
+use display::*;
 
 fn main() {
     // init
-    let (tx, rx) = channel();
+    let (key_tx, main_rx) = channel();
+    let gpio = Gpio::new().unwrap();
+    let display_data = Arc::new(Mutex::new(DisplayData::new()));
 
     // spawn threads
-    thread::spawn(move|| { keys_thread(tx) });
-    thread::spawn(led_display_thread);
-    main_thread(rx);
+    thread::spawn(move || keys_thread(key_tx));
+    thread::spawn(move || led_display_thread(&gpio, display_data.clone()));
+    main_thread(main_rx);
+    //main2();
 }
 
 // thread 1 : handle keystrokes
 // thread 2 : handle led matrix
 // master thread : handle everything else
 
-enum Key { Ok }
+enum Key {
+    Ok,
+}
 
 fn keys_thread(tx: mpsc::Sender<Key>) {
     loop {
@@ -31,22 +41,24 @@ fn keys_thread(tx: mpsc::Sender<Key>) {
     }
 }
 
-fn led_display_thread() {
-    loop {
-        // read time+options in a rwlock
-        // read dimm
-        // split by column
-        // loop display
-        // sleep
+fn led_display_thread(gpio: &Gpio, display_data: Arc<Mutex<DisplayData>>) {
+    { 
         let time = Local::now();
         println!("Time = {}", time.format("%H:%M:%S"));
-        sleep(Duration::from_millis(1000));
+        let mut data = display_data.lock().unwrap();
+        // didn't find a better way to get an integer
+        data.hours = time.format("%H").to_string().parse::<u8>().unwrap();
+        data.minutes = time.format("%M").to_string().parse::<u8>().unwrap();
+    }
+    let mut display = LedDisplay::new(gpio, display_data).unwrap();
+    loop {
+        display.show();
     }
 }
 
 fn main_thread(rx: mpsc::Receiver<Key>) {
     // wait for event : key, timeout
-    // keys : snooze 
+    // keys : snooze
     // keys : stop / start sound
     // keys : vol +
     // keys : vol -
